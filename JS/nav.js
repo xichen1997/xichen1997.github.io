965a4f806d017d2ab78160a05919217f4a294c0e
Xi Chen <chenxi_1997@tamu.edu>
2023-05-08 17:20:37 -0400

    The navbar will show up for all pages,  but it will meet some relative path issues.

--- JS/nav.js
+++ JS/nav.js
@@ -1,8 +1,31 @@
 // main.js
+
+
+
+// get root URL
+function getRootURL() {
+    const { protocol, host } = window.location;
+    return `${protocol}//${host}/`;
+}
+
+function loadCSSFile(filename) {
+    const rootURL = getRootURL();
+    const link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    link.href = rootURL + filename;
+    document.head.appendChild(link);
+}
+
+loadCSSFile('/css/style.css');
+
+// nav.js
 document.addEventListener('DOMContentLoaded', () => {
     const navbarContainer = document.getElementById('navbar');
+    const basePath = document.documentElement.getAttribute('data-base') || '/';
+    const navbarPath = basePath + 'Pages/navbar.html';
 
-    fetch('Pages/navbar.html')
+    fetch(navbarPath)
         .then(response => {
             if (!response.ok) {
                 throw new Error('Network response was not ok');
@@ -11,8 +34,18 @@ document.addEventListener('DOMContentLoaded', () => {
         })
         .then(data => {
             navbarContainer.innerHTML = data;
+            navbarContainer.addEventListener('click', handleNavLinkClick);
         })
         .catch(error => {
             console.error('Error fetching navbar:', error);
         });
 });
+
+function handleNavLinkClick(event) {
+    const link = event.target.closest('a[data-href]');
+    if (link) {
+        event.preventDefault();
+        const href = link.getAttribute('data-href');
+        window.location.href = href;
+    }
+}
